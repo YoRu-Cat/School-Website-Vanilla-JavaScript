df477ee0984425f8d329b9edf6acaa2850ecb88e
YoRu Tech <tech.niko.coding@gmail.com>
2024-05-21 11:32:31 +0500

    New content added

---
+++ main.js
@@ -0,0 +1,7 @@
+// change navbar style on scroll
+
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
