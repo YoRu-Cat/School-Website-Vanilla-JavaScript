8b92bce33f5ea859601f47a120561810a0f88a6b
YoRu Tech <tech.niko.coding@gmail.com>
2024-06-05 11:50:26 +0500

    FAQs

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+// show hide faq answer
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // change icon
+    const icon = faq.querySelector(".faq_icon i");
+    if (icon.className === "ri-add-large-line") {
+      icon.className = "ri-close-large-line";
+    } else {
+      icon.className = "ri-add-large-line";
+    }
+  });
+});
